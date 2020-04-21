cdcde1e25e72d62471cb2459af12ecca1758c249
Ilya <ilyalavrov97@gmail.com>
2020-04-21 19:04:14 +0300

    started

--- app/src/main/java/com/example/mobile_coding/ui/CatViewHolder.kt
+++ app/src/main/java/com/example/mobile_coding/ui/CatViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.mobile_coding.ui
 
 import android.view.View
+import com.bumptech.glide.Glide
 import com.example.mobile_coding.model.Cat
 import kotlinx.android.synthetic.main.item_cat.view.*
 
@@ -12,6 +13,11 @@ class CatViewHolder(
         itemView.run {
             catTextView.text = cat.getInfo()
             catAdditionalInfoTextView.text = cat.getAdditionalInfo()
+            Glide
+                .with(context)
+                .load("https://ichef.bbci.co.uk/news/410/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg")
+                .centerCrop()
+                .into(catImageView)
         }
     }
 }
